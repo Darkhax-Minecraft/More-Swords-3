8584412ee99d8c7e3650692c6f182d477c93fd57
Tyler Hancock <darklime@live.ca>
2013-11-15 13:46:06 -0700

    Hooked up proxy system.

--- net/darkhax/moreswords/MoreSwords.java
+++ net/darkhax/moreswords/MoreSwords.java
@@ -1,8 +1,10 @@
 package net.darkhax.moreswords;
 
+import nat.darkhax.moreswords.core.proxy.CommonProxy;
 import net.darkhax.moreswords.lib.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -11,6 +13,9 @@ import cpw.mods.fml.common.network.NetworkMod;
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_NUMBER)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class MoreSwords {
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+	public static CommonProxy proxy;
 	
 	@Mod.Instance(Reference.MOD_ID)
 	public static MoreSwords instance;
