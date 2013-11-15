3de929d1191dfd5c60d8240cdc16da4f5a578ede
Tyler Hancock <darklime@live.ca>
2013-11-15 13:41:35 -0700

    Added version number.

--- net/darkhax/moreswords/lib/Reference.java
+++ net/darkhax/moreswords/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "MSM3";
 	public static final String MOD_NAME = "More Swords 3";
-	public static final String VERSION_NUMBER = "";
+	public static final String VERSION_NUMBER = "3.0 snapshot 0.0";
 	public static final String CLIENT_PROXY_CLASS = "net.darkhax.moreswords.core.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.darkhax.moreswords.core.proxy.CommonProxy";
 }
