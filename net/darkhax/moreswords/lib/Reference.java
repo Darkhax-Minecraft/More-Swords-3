9ead366841495b013ad6966708f0253428cc06db
Tyler Hancock <darklime@live.ca>
2013-11-15 13:36:24 -0700

    Created reference class. Filled with stuff.

---
+++ net/darkhax/moreswords/lib/Reference.java
@@ -0,0 +1,10 @@
+package net.darkhax.moreswords.lib;
+
+public class Reference {
+
+	public static final String MOD_ID = "MSM3";
+	public static final String MOD_NAME = "More Swords 3";
+	public static final String VERSION_NUMBER = "";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String SERVER_PROXY_CLASS = "";
+}
