4cbec1920164358dcc6c4f81e1c071ec685b46eb
Tyler Hancock <darklime@live.ca>
2013-11-15 14:07:35 -0700

    Created base sword.

---
+++ net/darkhax/moreswords/item/ItemCoreSword.java
@@ -0,0 +1,12 @@
+package net.darkhax.moreswords.item;
+
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.ItemSword;
+
+public class ItemCoreSword extends ItemSword {
+
+	public ItemCoreSword(int id, EnumToolMaterial material) {
+	
+		super(id, material);
+	}
+}
