cfcbafb103b022767922109ac6191a10db71e545
Tyler Hancock <darklime@live.ca>
2013-11-15 21:53:09 -0700

    Created the eleven initial swords. Not yet fully integrated into the enum or config system.

--- net/darkhax/moreswords/item/Items.java
+++ net/darkhax/moreswords/item/Items.java
@@ -1,7 +1,22 @@
 package net.darkhax.moreswords.item;
 
+import net.minecraft.item.EnumToolMaterial;
+import net.minecraft.item.Item;
+
 public class Items {
 
+	public static Item swordDawmStar;
+	public static Item swordVampiric;
+	public static Item swordGladiolus;
+	public static Item swordDraconic;
+	public static Item swordEnder;
+	public static Item swordCrystal;
+	public static Item swordInfinity;
+	public static Item swordGlacial;
+	public static Item swordAether;
+	public static Item swordWither;
+	public static Item swordAdmin;
+	
 	public Items() {
 		
 		addItems();
@@ -9,5 +24,16 @@ public class Items {
 	
 	void addItems() {
 		
+		swordDawmStar = new ItemCoreSword(13400, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.dawnstar");
+		swordVampiric = new ItemCoreSword(13401, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.vampiric");
+		swordGladiolus = new ItemCoreSword(13402, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.gladiolus");
+		swordDraconic = new ItemCoreSword(13403, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.draconic");
+		swordEnder = new ItemCoreSword(13404, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.ender");
+		swordCrystal = new ItemCoreSword(13405, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.crystal");
+		swordInfinity = new ItemCoreSword(13406, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.infinity");
+		swordGlacial = new ItemCoreSword(13407, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.glacial");
+		swordAether = new ItemCoreSword(13408, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.aether");
+		swordWither = new ItemCoreSword(13409, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.wither");
+		swordAdmin = new ItemCoreSword(13410, EnumToolMaterial.EMERALD).setUnlocalizedName("msm.admin");
 	}
 }
