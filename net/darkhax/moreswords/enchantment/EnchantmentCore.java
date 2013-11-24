9aa797447d2841fe5859a7ee3043474cc8823d29
Tyler Hancock <darklime@live.ca>
2013-11-24 16:33:53 -0700

    Added everything needed for the enchantments.

--- net/darkhax/moreswords/enchantment/EnchantmentCore.java
+++ net/darkhax/moreswords/enchantment/EnchantmentCore.java
@@ -1,12 +1,61 @@
 package net.darkhax.moreswords.enchantment;
 
+import net.darkhax.moreswords.lib.EnumManager;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBook;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
 
 public class EnchantmentCore extends Enchantment {
 
-	protected EnchantmentCore(int id, int weight, EnumEnchantmentType type) {
+	int maxLevel;
+	int minLevel;
+	ItemStack stack;
+	
+	/**
+	 * @param id: ID for the enchantment being added.
+	 * @param weight: How often the enchantment shows up.
+	 * @param type: Type of enchantment, defaulted to more swords custom type.
+	 * @param minLevel: The lowest possible level of enchantment.
+	 * @param maxLevel: The highest possible level of enchantment.
+	 * @param itemStack: Stack that can get this enchantment. Books added by default.
+	 */
+	protected EnchantmentCore(int id, int weight, EnumEnchantmentType type, int minLevel, int maxLevel, ItemStack itemStack) {
 		
-		super(id, weight, type);
+		super(id, weight, EnumManager.enumSwords);
+		this.minLevel = minLevel;
+		this.maxLevel = maxLevel;
 	}
+
+    public int getMinLevel() {
+    	
+        return this.minLevel;
+    }
+
+    public int getMaxLevel() {
+    	
+        return this.maxLevel;
+    }
+    
+	public boolean canApplyAtEnchantingTable(ItemStack stack) {
+		
+		if (stack.getItem() == this.stack.getItem() | stack.getItem() == Item.book) {
+			
+			return true;
+		}
+		
+		else return false;
+	}
+	
+    public boolean canApply(ItemStack stack) {
+    	
+    	if (stack.getItem() instanceof ItemSword | stack.getItem() == Item.book) {
+    			
+    		return true;
+    	}	
+    	
+    	else return false;
+    }
 }
