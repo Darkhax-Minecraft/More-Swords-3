2216c80f6d34f83516b23fc130d08bee3784123b
Tyler Hancock <darklime@live.ca>
2014-03-13 17:03:20 -0600

    Fixed proxy error.

--- src/net/darkhax/moreswords/lib/Reference.java
+++ src/net/darkhax/moreswords/lib/Reference.java
@@ -5,8 +5,8 @@ public class Reference {
 	public static final String MOD_ID = "MSM3";
 	public static final String MOD_NAME = "More Swords 3";
 	public static final String VERSION_NUMBER = "3.0 snapshot";
-	public static final String CLIENT_PROXY_CLASS = "net.darkhax.moreswords.core.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "net.darkhax.moreswords.core.proxy.CommonProxy";
+	public static final String CLIENT_PROXY_CLASS = "net.darkhax.moreswords.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "net.darkhax.moreswords.proxy.CommonProxy";
 	public static final String TEXTURE_DOMAIN = "moreswords:";
 	
 	public static RandomUtils random = new RandomUtils();
