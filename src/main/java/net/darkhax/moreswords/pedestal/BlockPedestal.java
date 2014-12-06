7b5fb7d55686393b3efb8400b4fbc5c513d6b2b7
Darkhax <darklime@live.ca>
2014-12-05 21:03:30 -0700

    1.8 stuffs.

---
+++ src/main/java/net/darkhax/moreswords/pedestal/BlockPedestal.java
@@ -0,0 +1,12 @@
+package net.darkhax.moreswords.pedestal;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockPedestal extends Block {
+
+    public BlockPedestal() {
+        
+        super(Material.rock);
+    }
+}
