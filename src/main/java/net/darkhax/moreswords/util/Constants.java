22624f74456fe8a10582c78c95d9b974e85cb515
Darkhax <darklime@live.ca>
2016-03-18 02:18:16 -0600

    Updated to use build server.

--- src/main/java/net/darkhax/moreswords/util/Constants.java
+++ src/main/java/net/darkhax/moreswords/util/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
     
     public static final String ID = "MSM3";
     public static final String NAME = "More Swords 3";
-    public static final String VERSION = "3.0.0";
+    public static final String VERSION = "3.1.0.0";
     public static final String SERVER_PROXY = "net.darkhax.moreswords.common.CommonProxy";
     public static final String CLIENT_PROXY = "net.darkhax.moreswords.client.ClientProxy";
     public static final String FACTORY = "net.darkhax.moreswords.client.gui.GuiFactoryMoreSwords";
