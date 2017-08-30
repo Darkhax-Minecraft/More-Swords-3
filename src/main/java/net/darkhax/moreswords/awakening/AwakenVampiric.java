87ebb50c9a3a76931bab3f82ca1605eff472650e
Tyler Hancock <darklime@live.ca>
2017-08-29 18:25:20 -0600

    Added awakening ritual for the Vampiric Blade

--- src/main/java/net/darkhax/moreswords/awakening/AwakenVampiric.java
+++ src/main/java/net/darkhax/moreswords/awakening/AwakenVampiric.java
@@ -1,15 +1,48 @@
 package net.darkhax.moreswords.awakening;
 
+import net.darkhax.bookshelf.util.MathsUtils;
+import net.darkhax.bookshelf.util.NBTUtils;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class AwakenVampiric extends Awakening {
+
+    private static final String TAG_VAMP_Kills = "VampireKills";
+    private final int requiredKills = 10;
 
     @Override
     public int getAwakenProgress (EntityLivingBase entity, ItemStack stack, NBTTagCompound tag) {
 
-        // TODO Auto-generated method stub
-        return 0;
+        return MathsUtils.getPercentage(NBTUtils.getAmount(stack, TAG_VAMP_Kills), this.requiredKills);
+    }
+
+    @SubscribeEvent
+    public void onMobKilled (LivingDeathEvent event) {
+
+        final Entity source = event.getSource().getTrueSource();
+
+        // Prevents non-hostile mobs from counting.
+        if (source instanceof EntityLivingBase && event.getEntity() instanceof IMob) {
+
+            final EntityLivingBase living = (EntityLivingBase) source;
+
+            // Makes sure the player has the right held item. And only one health.
+            if (living.getHealth() <= 1f && !living.isDead && this.isValidItem(living.getHeldItemMainhand())) {
+
+                // Adds one vampiric kill
+                NBTUtils.increment(living.getHeldItemMainhand(), TAG_VAMP_Kills, 1);
+
+                // Awakens the item
+                if (this.canAwaken(living, living.getHeldItemMainhand())) {
+
+                    this.awaken(living);
+                }
+            }
+        }
     }
 }
