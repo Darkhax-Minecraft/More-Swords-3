667c5b2d1f743d52d0569f4d9fa1da1afeb219db
Tyler Hancock <darklime@live.ca>
2017-08-29 19:25:17 -0600

    Added an awakening for the crystaline blade.

--- src/main/java/net/darkhax/moreswords/awakening/AwakenCrystal.java
+++ src/main/java/net/darkhax/moreswords/awakening/AwakenCrystal.java
@@ -1,15 +1,37 @@
 package net.darkhax.moreswords.awakening;
 
+import net.darkhax.bookshelf.util.MathsUtils;
+import net.darkhax.bookshelf.util.NBTUtils;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class AwakenCrystal extends Awakening {
+
+    private static final String TAG_EXPERIENCE = "Experience";
+    private final int requiredExp = 170;
 
     @Override
     public int getAwakenProgress (EntityLivingBase entity, ItemStack stack, NBTTagCompound tag) {
 
-        // TODO Auto-generated method stub
-        return 0;
+        return MathsUtils.getPercentage(NBTUtils.getAmount(stack, TAG_EXPERIENCE), this.requiredExp);
+    }
+
+    @SubscribeEvent
+    public void onPlayerPickupEXP (PlayerPickupXpEvent event) {
+
+        if (!event.getOrb().isDead && this.isValidItem(event.getEntityPlayer().getHeldItemMainhand())) {
+
+            event.setCanceled(true);
+            event.getOrb().setDead();
+            NBTUtils.increment(event.getEntityPlayer().getHeldItemMainhand(), TAG_EXPERIENCE, event.getOrb().xpValue);
+
+            if (this.canAwaken(event.getEntityPlayer(), event.getEntityPlayer().getHeldItemMainhand())) {
+
+                this.awaken(event.getEntityPlayer());
+            }
+        }
     }
 }
