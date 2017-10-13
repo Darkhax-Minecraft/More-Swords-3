6ef8eaaebb2b924a5d9889a19a9c15a287cc131e
Tyler Hancock <darklime@live.ca>
2017-10-12 23:46:58 -0600

    Added crystaline effects.

--- src/main/java/net/darkhax/moreswords/awakening/AwakenCrystal.java
+++ src/main/java/net/darkhax/moreswords/awakening/AwakenCrystal.java
@@ -3,8 +3,11 @@ package net.darkhax.moreswords.awakening;
 import net.darkhax.bookshelf.util.MathsUtils;
 import net.darkhax.bookshelf.util.NBTUtils;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.event.entity.living.LivingExperienceDropEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.player.PlayerPickupXpEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -12,7 +15,22 @@ public class AwakenCrystal extends Awakening {
 
     private static final String TAG_EXPERIENCE = "Experience";
     private final int requiredExp = 170;
-
+    
+    @SubscribeEvent
+    public void onEXPCalc(LivingExperienceDropEvent event) {
+    	
+    	if (this.getMaterial().isPlayerHolding(event.getAttackingPlayer())) {
+    		
+    		event.setDroppedExperience(event.getDroppedExperience() + event.getOriginalExperience());
+    	}
+    }
+    
+    @Override
+    public void onHolderAttack(EntityPlayer holder, EntityLivingBase victim, ItemStack stack, LivingHurtEvent event) {
+    	
+    	event.setAmount(event.getAmount() + Math.max((holder.experienceLevel / 5), 10f));
+    }
+    
     @Override
     public int getAwakenProgress (EntityLivingBase entity, ItemStack stack, NBTTagCompound tag) {
 
